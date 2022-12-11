6887ae48ceb59c789930f748d35432954f1453c0
dk1a <dk1a@protonmail.com>
2022-12-11 16:17:52 +0300

    fix: fix critical issues in SliceIter tests

--- test/SliceIter.t.sol
+++ test/SliceIter.t.sol
@@ -21,6 +21,10 @@ contract SliceIterTest is PRBTest {
         assertFalse(new bytes(1).toSlice().iter().isEmpty());
     }
 
+    /*//////////////////////////////////////////////////////////////////////////
+                                    NEXT
+    //////////////////////////////////////////////////////////////////////////*/
+
     function testNext() public {
         Slice s = bytes("123").toSlice();
         SliceIter memory iter = s.iter();
@@ -31,6 +35,18 @@ contract SliceIterTest is PRBTest {
         assertEq(iter.asSlice().toBytes(), bytes("3"));
         assertEq(iter.next(), uint8(bytes1("3")));
         assertEq(iter.asSlice().toBytes(), bytes(""));
+
+        vm.expectRevert(SliceIter__StopIteration.selector);
+        iter.next();
+    }
+
+    function testNext__StopIteration() public {
+        Slice s = bytes("123").toSlice();
+        SliceIter memory iter = s.iter();
+
+        iter.next();
+        iter.next();
+        iter.next();
 
         vm.expectRevert(SliceIter__StopIteration.selector);
         iter.next();
@@ -50,6 +66,23 @@ contract SliceIterTest is PRBTest {
         iter.next();
     }
 
+    function testNext__StopIteration__Fuzz(bytes calldata _b) public {
+        SliceIter memory iter = _b.toSlice().iter();
+
+        uint256 i;
+        while (!iter.isEmpty()) {
+            iter.next();
+            i++;
+        }
+
+        vm.expectRevert(SliceIter__StopIteration.selector);
+        iter.next();
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                    NEXT_BACK
+    //////////////////////////////////////////////////////////////////////////*/
+
     function testNextBack() public {
         Slice s = bytes("123").toSlice();
         SliceIter memory iter = s.iter();
@@ -62,7 +95,19 @@ contract SliceIterTest is PRBTest {
         assertEq(iter.asSlice().toBytes(), bytes(""));
 
         vm.expectRevert(SliceIter__StopIteration.selector);
-        iter.next();
+        iter.nextBack();
+    }
+
+    function testNextBack__StopIteration() public {
+        Slice s = bytes("123").toSlice();
+        SliceIter memory iter = s.iter();
+
+        iter.nextBack();
+        iter.nextBack();
+        iter.nextBack();
+        
+        vm.expectRevert(SliceIter__StopIteration.selector);
+        iter.nextBack();
     }
 
     function testNextBack__Fuzz(bytes calldata _b) public {
@@ -70,11 +115,55 @@ contract SliceIterTest is PRBTest {
 
         uint256 i;
         while (!iter.isEmpty()) {
-            assertEq(iter.next(), uint8(_b[_b.length - i - 1]));
-            assertEq(iter.asSlice().toBytes(), _b[:_b.length - i]);
+            assertEq(iter.nextBack(), uint8(_b[_b.length - i - 1]));
+            assertEq(iter.asSlice().toBytes(), _b[:_b.length - i - 1]);
+            i++;
+        }
+    }
+
+    function testNextBack__StopIteration__Fuzz(bytes calldata _b) public {
+        SliceIter memory iter = _b.toSlice().iter();
+
+        uint256 i;
+        while (!iter.isEmpty()) {
+            iter.nextBack();
             i++;
         }
 
+        vm.expectRevert(SliceIter__StopIteration.selector);
+        iter.nextBack();
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                    NEXT MIXED
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function testNextMixed() public {
+        Slice s = bytes("12345").toSlice();
+        SliceIter memory iter = s.iter();
+
+        assertEq(iter.next(), uint8(bytes1("1")));
+        assertEq(iter.asSlice().toBytes(), bytes("2345"));
+        assertEq(iter.nextBack(), uint8(bytes1("5")));
+        assertEq(iter.asSlice().toBytes(), bytes("234"));
+        assertEq(iter.next(), uint8(bytes1("2")));
+        assertEq(iter.asSlice().toBytes(), bytes("34"));
+        assertEq(iter.next(), uint8(bytes1("3")));
+        assertEq(iter.asSlice().toBytes(), bytes("4"));
+        assertEq(iter.nextBack(), uint8(bytes1("4")));
+        assertEq(iter.asSlice().toBytes(), bytes(""));
+    }
+
+    function testNextMixed__StopIteration() public {
+        Slice s = bytes("12345").toSlice();
+        SliceIter memory iter = s.iter();
+
+        iter.next();
+        iter.nextBack();
+        iter.next();
+        iter.next();
+        iter.nextBack();
+        
         vm.expectRevert(SliceIter__StopIteration.selector);
         iter.next();
     }
